e07ed4827d1b6205087eb8dc3d294c43d319bbfb
Ellie Lambert <ebezjian@gmail.com>
2020-11-05 14:57:45 -0500

    add functionality and some imagry
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
-//User Interface Logic
-
-
 //Business Logic
 
 function Player(name) {
   this.name = name;
   this.totalScore = 0;
   this.roundScore = 0;
-  this.dice = 0; 
+  this.dice1 = 0; 
+  this.dice2 = 0;
   this.hold = false;
 }
 
@@ -17,16 +15,17 @@ let playerTwo = new Player("Player 2");
 let isPlayerOne = true;
 
 Player.prototype.diceRoll = function () {
-  this.dice = Math.floor(Math.random() * 6) + 1;
-  if (this.dice === 1) {
-    console.log("Your turn is over");
+  this.dice1 = Math.floor(Math.random() * 6) + 1;
+  this.dice2 = Math.floor(Math.random() * 6) + 1;
+  if (this.dice1 === 1 && this.dice2 === 1) {
+    this.roundScore += this.dice1 + this.dice2 + 25;
+  } else if (this.dice1 === 1 || this.dice2 === 1) {
+    console.log("You rolled a 1. Your turn is over.");
     this.roundScore = 0;
     isPlayerOne = !isPlayerOne;
-  } else if (this.dice !==1 && this.hold === false) {
-    this.roundScore += this.dice;
-    if(this.totalScore + this.roundScore >= 100) {
-      declareWinner(this.name);
-    }
+  } else if (this.dice1 != 1 && this.dice2 != 1 && this.hold === false) {
+    this.roundScore += this.dice1 + this.dice2;
+    console.log(this.roundScore);
     return;
   }
 }
@@ -36,34 +35,48 @@ Player.prototype.holdDice = function () {
     this.totalScore += this.roundScore;
     this.roundScore = 0;
     isPlayerOne = !isPlayerOne;
+    this.hold = false;
     return;
   }
 }
 
 function turnTest() {
-  if (isPlayerOne == true) {
+  if (isPlayerOne === true) {
     playerOne.diceRoll();
   } else {
     playerTwo.diceRoll();
   }
+  checkForWinner();
 }
 
 // UI Logic
 
-function declareWinner(element) {
-  console.log(element + " wins");
+function checkForWinner() {
+  if (playerOne.totalScore >= 100) {
+    playerOne.declareWinner(playerOne.name);
+    $(".winner").append("Player One");
+  } else if (playerTwo.totalScore >= 100) {
+    playerTwo.declareWinner(playerTwo.name);
+    $(".winner").append("Player Two");
+  }
 }
 
-
+Player.prototype.declareWinner = function(name) {
+  playerOne.roundScore = 0;
+  playerOne.totalScore = 0;
+  playerTwo.roundScore = 0;
+  playerTwo.totalScore = 0;
+  return;
+}
 
 $(document).ready(function() {
   $("button#roll").click(function(event) {
     event.preventDefault();
+    turnTest();
     $(".player1total").text(playerOne.totalScore);
     $(".player1round").text(playerOne.roundScore);
     $(".player2total").text(playerTwo.totalScore);
     $(".player2round").text(playerTwo.roundScore);
-    turnTest();
     });
   $("button#hold").click(function(event) {
     event.preventDefault();
